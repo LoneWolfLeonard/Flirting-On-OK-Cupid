9fa74495c5c452e28fe6c6200a6d3d048f508fe4
Leonard Treman <leonardtreman@gmail.com>
2017-04-30 11:53:35 -0400

    Add files via upload

---
+++ CodeOfLove.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package code.of.love;
+
+/**
+ *
+ * @author leonard
+ */
+public class CodeOfLove {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Public class lovetest 
+{
+Public Static Void Main String args[] 
+{ 
+String EpicGuy = "Leonard"; 
+String EpicGirl = "Cmullinix"; 
+Boolean hangout = false; 
+int impression = 0; 
+int worth it = 100; 
+int likesanime = 5; 
+int likesmobas = 2; 
+int flirtycomment =0; 
+int yourphonenumber = null;
+while(impression != worthit) 
+{ 
+impression = flirtycomment; 
+flirtycomment = flirtycomment + 1; 
+} 
+hangout = true; 
+if (hangout == true) 
+{ 
+return yourphonenumber; 
+} 
+} 
+}
+    }
+    
+}
